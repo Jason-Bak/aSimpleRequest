35becaf30277562c91981774355241107c6feae3
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2024-02-03 18:27:13 -0500

    Delete Valentines Day Website directory

--- Valentines Day Website/html/letter.js
+++
@@ -1,4 +0,0 @@
-function yes() {
-    window.location.href = "https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NHZvbzBhbGVucG8yMDFtdTlzMTM3cGFtMmMgamFzb25iYWswMkBt&tmsrc=jasonbak02%40gmail.com";
-}
-
