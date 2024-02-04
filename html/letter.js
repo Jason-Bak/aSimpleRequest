244b606f5ebb31f02b0c5df29c36b37d9f76296b
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2024-02-03 19:55:50 -0500

    Update letter.js

--- html/letter.js
+++ html/letter.js
@@ -1,4 +1,12 @@
+var letter = document.getElementById("letter");
+var msg = document.getElementById("msg");
+
 function yes() {
     window.location.href = "https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NHZvbzBhbGVucG8yMDFtdTlzMTM3cGFtMmMgamFzb25iYWswMkBt&tmsrc=jasonbak02%40gmail.com";
 }
 
+document.addEventListener('click', function(e){
+    document.getElementById('audio').play();
+    msg.style.visibility='hidden';
+    letter.style.visibility='visible';
+});
